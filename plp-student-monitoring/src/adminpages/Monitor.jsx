2b7cefa76aa796da63567c3b1281e33f9b50b2ce
jalzoren <flavierlaurence01@gmail.com>
2026-02-19 23:31:50 +0800

    Done creating missile

--- plp-student-monitoring/src/adminpages/Monitor.jsx
+++ plp-student-monitoring/src/adminpages/Monitor.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import '../css/Monitor.css'; // Create this CSS file for styling
 function Monitor() {
   return (
-    <div>Monitor</div>
-  )
+    <div>
+<header className="header-card">
+  <h1>REAL-TIME MONITOR</h1>
+  
+  
+  <p className="subtitle">Dashboard / Real-Time Monitor</p>
+
+</header>
+    <hr className="header-divider"></hr>
+
+
+    </div>
+
+ )
 }
 
 export default Monitor
--- plp-student-monitoring/src/adminpages/Records.jsx
+++ plp-student-monitoring/src/adminpages/Records.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import '../css/Records.css'; // Create this CSS file for styling
 function Records() {
   return (
-    <div>Records</div>
-  )
+    <div>
+<header className="header-card">
+  <h1>ENTRY-EXIT RECORDS</h1>
+  
+  
+  <p className="subtitle">Dashboard / Entry-Exit Records</p>
+
+</header>
+    <hr className="header-divider"></hr>
+
+
+    </div>
+
+ )
 }
 
 export default Records
