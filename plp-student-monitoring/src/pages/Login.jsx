dc71c87e8e80e4194986c020399757951c72b3ca
LynnCzyla <lynnzylameczdo@gmail.com>
2026-02-22 14:10:32 +0800

    tempory login done

--- plp-student-monitoring/src/pages/Login.jsx
+++ plp-student-monitoring/src/pages/Login.jsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import "../css/Login.css";
 import logo from "../assets/logo2.png";
 import { LuScanFace } from "react-icons/lu";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
     console.log({ username, password });
     // Later: call real login API here
+
+    navigate("/dashboard");
   };
 
   return (
