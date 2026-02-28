b90111120e2d1f087ea76c4c87e64e5918c7283a
jerimiahbitancor <jerimiahbitancor27@gmail.com>
2026-02-28 12:55:02 +0800

    .

--- python/venv/Scripts/activate.bat
+++ python/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Entrance-and-Exit-Monitoring-PLP-Students\python\venv
+set VIRTUAL_ENV=D:\Codes\Entry Exit Monitoring Students\Entrance-and-Exit-Monitoring-PLP-Students\python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
